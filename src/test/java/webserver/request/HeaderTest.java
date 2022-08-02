302bc636aece4349af8cadbb0694b75074acffb0
kkwan0226 <kkwan0226@gmail.com>
2022-08-02 20:06:14 +0900

    :white_check_mark: test: Header 테스트 구현

---
+++ src/test/java/webserver/request/HeaderTest.java
@@ -0,0 +1,23 @@
+package webserver.request;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class HeaderTest {
+
+    @Test
+    void 주어진_HEADER_LINE_LIST로부터_HEADER_파싱_테스트() {
+        List<String> headerLineList = List.of("Host: localhost:8080", "Connection: keep-alive", "Accept: */*");
+        Header header = Header.from(headerLineList);
+
+        assertAll(
+                () -> assertThat(header.get("Host")).isEqualTo("localhost:8080"),
+                () -> assertThat(header.get("Connection")).isEqualTo("keep-alive"),
+                () -> assertThat(header.get("Accept")).isEqualTo("*/*")
+        );
+    }
+}
