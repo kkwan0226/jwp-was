917c375aa59f86b87064cb0513779c3b6be50dbc
kkwan0226 <kkwan0226@gmail.com>
2022-08-02 19:38:40 +0900

    :sparkles: feat: IOUtils.readMultiLine() 구현

--- src/main/java/utils/IOUtils.java
+++ src/main/java/utils/IOUtils.java
@@ -2,19 +2,25 @@ package utils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class IOUtils {
-    /**
-     * @param BufferedReader는
-     *            Request Body를 시작하는 시점이어야
-     * @param contentLength는
-     *            Request Header의 Content-Length 값이다.
-     * @return
-     * @throws IOException
-     */
+
     public static String readData(BufferedReader br, int contentLength) throws IOException {
         char[] body = new char[contentLength];
         br.read(body, 0, contentLength);
         return String.copyValueOf(body);
+    }
+
+    public static List<String> readMultiLine(BufferedReader bufferedReader) throws IOException {
+        List<String> multiLine = new ArrayList<>();
+        String line;
+
+        while ((line = bufferedReader.readLine()) != null && !line.isEmpty()) {
+            multiLine.add(line);
+        }
+
+        return multiLine;
     }
 }
