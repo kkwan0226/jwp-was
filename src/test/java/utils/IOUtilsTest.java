8c44d874025df7c69d5e1d2edea6d8c9bfdda77a
kkwan0226 <kkwan0226@gmail.com>
2022-08-02 19:38:02 +0900

    :white_check_mark: test: IOUtils.readMultiLine() 테스트 구현

--- src/test/java/utils/IOUtilsTest.java
+++ src/test/java/utils/IOUtilsTest.java
@@ -1,11 +1,16 @@
 package utils;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.StringReader;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 public class IOUtilsTest {
     private static final Logger logger = LoggerFactory.getLogger(IOUtilsTest.class);
@@ -17,5 +22,21 @@ public class IOUtilsTest {
         BufferedReader br = new BufferedReader(sr);
 
         logger.debug("parse body : {}", IOUtils.readData(br, data.length()));
+    }
+
+    @Test
+    public void 올바른_BufferedReader_테스트() throws Exception {
+        BufferedReader bufferedReader = Mockito.mock(BufferedReader.class);
+        String[] expected = {"GET /index.html HTTP/1.1", "Host: localhost:8080", "Connection: keep-alive", "Accept: */*", null};
+        when(bufferedReader.readLine())
+                .thenReturn("GET /index.html HTTP/1.1")
+                .thenReturn("Host: localhost:8080")
+                .thenReturn("Connection: keep-alive")
+                .thenReturn("Accept: */*")
+                .thenReturn(null);
+
+        List<String> multiLine = IOUtils.readMultiLine(bufferedReader);
+
+        assertThat(multiLine).containsExactly(expected);
     }
 }
